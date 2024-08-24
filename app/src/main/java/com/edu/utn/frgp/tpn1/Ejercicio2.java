3d3fc1f31d90e58dfc1bd63f0571239f1735b551
Emiliano Cicarelli <emilianocicarelli@gmail.com>
2024-08-24 19:50:00 -0300

    Se agrega el borrar y se cambian algunos parametros para cumplimentar con este punto.

--- app/src/main/java/com/edu/utn/frgp/tpn1/Ejercicio2.java
+++ app/src/main/java/com/edu/utn/frgp/tpn1/Ejercicio2.java
@@ -1,11 +1,9 @@
 package com.edu.utn.frgp.tpn1;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,34 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Ejercicio2 extends AppCompatActivity {
-    private Button cero;
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button four;
-    private Button five;
-    private Button six;
-    private Button seven;
-    private Button eight;
-    private Button nine;
-    private Button add;
-    private Button sub;
-    private Button mul;
-    private Button div;
-    private Button equal;
-    private Button borrar;
-    private TextView info;
-    private TextView result;
-    private final char suma = '+';
-    private final char resta = '-';
-    private final char multiplicar= '*';
-    private final char dividir = '/';
-    private final char EQU = 0;
+    private Button cero, one, two, three, four, five, six, seven, eight, nine;
+    private Button add, sub, mul, div, equal, borrar;
+    private TextView info, result;
     private double val1 = Double.NaN;
     private double val2;
     private char accion;
+    private boolean isCalculating = false;
+    private boolean isResultDisplayed = false;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -52,203 +32,152 @@ public class Ejercicio2 extends AppCompatActivity {
         });
 
         setupUIViews();
-
-        cero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "0");
-            }
-        });
-
-        one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "1");
-            }
-        });
-
-        two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "2");
-            }
-        });
-
-        three.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "3");
-            }
-        });
-
-        four.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "4");
-            }
-        });
-
-        five.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "5");
-            }
-        });
-
-        six.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "6");
-            }
-        });
-
-        seven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "7");
-            }
-        });
-
-        eight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "8");
-            }
-        });
-
-        nine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                info.setText(info.getText().toString() + "9");
-            }
-        });
-
-        sub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (info.getText().toString().isEmpty()) {
-                    // Si no hay número en la pantalla, permite el ingreso de un número negativo
-                    info.setText("-");
-                } else {
-                    calcular();
-                    accion = resta;
-                    result.setText(String.valueOf(val1) + "-");
-                    info.setText(null);
-                }
-            }
-        });
-
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcular();
-                accion = suma;
-                result.setText(String.valueOf(val1) + "+");
-                info.setText(null);
-            }
-        });
-
-        mul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcular();
-                accion = multiplicar;
-                result.setText(String.valueOf(val1) + "*");
-                info.setText(null);
-            }
-        });
-
-        div.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcular();
-                accion = dividir;
-                result.setText(String.valueOf(val1) + "/");
-                info.setText(null);
-            }
-        });
-
-        equal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcular();
-                accion = EQU;
-                result.setText(result.getText().toString() + String.valueOf(val2) + "=" + String.valueOf(val1));
-                // 5 + 4 = 9
-                info.setText(null);
-            }
-        });
-
-        borrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(info.getText().length() > 0){
-                    CharSequence name = info.getText().toString();
-                    info.setText(name.subSequence(0, name.length()-1));
-                }
-                else{
-                    val1 = Double.NaN;
-                    val2 = Double.NaN;
-                    info.setText(null);
-                    result.setText(null);
-                }
-            }
-        });
-    }
-    private void setupUIViews(){
-
-        one = (Button)findViewById(R.id.btn1);
-        cero = (Button)findViewById(R.id.btn0);
-        two = (Button)findViewById(R.id.btn2);
-        three = (Button)findViewById(R.id.btn3);
-        four = (Button)findViewById(R.id.btn4);
-        five = (Button)findViewById(R.id.btn5);
-        six = (Button)findViewById(R.id.btn6);
-        seven = (Button)findViewById(R.id.btn7);
-        eight = (Button)findViewById(R.id.btn8);
-        nine = (Button)findViewById(R.id.btn9);
-        add = (Button)findViewById(R.id.btnadd);
-        sub = (Button)findViewById(R.id.btnsub);
-        mul = (Button)findViewById(R.id.btnmul);
-        div = (Button)findViewById(R.id.btndivide);
-        equal = (Button)findViewById(R.id.btnequal);
-        info = (TextView)findViewById(R.id.tvControl);
-        result = (TextView)findViewById(R.id.tvResult);
-        borrar = (Button)findViewById(R.id.btnborrar);
+        setupButtonListeners();
     }
 
-    private void calcular(){
-        if(!Double.isNaN(val1)){
-            val2 = Double.parseDouble(info.getText().toString());
+    private void setupUIViews() {
+        one = findViewById(R.id.btn1);
+        cero = findViewById(R.id.btn0);
+        two = findViewById(R.id.btn2);
+        three = findViewById(R.id.btn3);
+        four = findViewById(R.id.btn4);
+        five = findViewById(R.id.btn5);
+        six = findViewById(R.id.btn6);
+        seven = findViewById(R.id.btn7);
+        eight = findViewById(R.id.btn8);
+        nine = findViewById(R.id.btn9);
+        add = findViewById(R.id.btnadd);
+        sub = findViewById(R.id.btnsub);
+        mul = findViewById(R.id.btnmul);
+        div = findViewById(R.id.btndivide);
+        equal = findViewById(R.id.btnequal);
+        info = findViewById(R.id.tvControl);
+        result = findViewById(R.id.tvResult);
+        borrar = findViewById(R.id.btnborrar);
+    }
 
-            if (accion == dividir && val2 == 0) {
-                // Mostrar un Toast si se intenta dividir por 0
-                Toast.makeText(Ejercicio2.this, "Error: División por 0 no permitida", Toast.LENGTH_LONG).show();
-                info.setText(null);  // Limpiar la pantalla de entrada
-                result.setText(null);  // Limpiar el resultado
-                val1 = Double.NaN;  // Resetear val1 para evitar cálculos incorrectos
-                accion = EQU;  // Resetear la acción
-                return;  // Salir del método para evitar que continúe con el cálculo
+    private void setupButtonListeners() {
+        cero.setOnClickListener(v -> handleNumberInput("0"));
+        one.setOnClickListener(v -> handleNumberInput("1"));
+        two.setOnClickListener(v -> handleNumberInput("2"));
+        three.setOnClickListener(v -> handleNumberInput("3"));
+        four.setOnClickListener(v -> handleNumberInput("4"));
+        five.setOnClickListener(v -> handleNumberInput("5"));
+        six.setOnClickListener(v -> handleNumberInput("6"));
+        seven.setOnClickListener(v -> handleNumberInput("7"));
+        eight.setOnClickListener(v -> handleNumberInput("8"));
+        nine.setOnClickListener(v -> handleNumberInput("9"));
+
+        add.setOnClickListener(v -> handleOperatorInput('+'));
+        sub.setOnClickListener(v -> handleOperatorInput('-'));
+        mul.setOnClickListener(v -> handleOperatorInput('*'));
+        div.setOnClickListener(v -> handleOperatorInput('/'));
+
+        equal.setOnClickListener(v -> {
+            if (!isResultDisplayed) {
+                calculateResult();
+            } else {
+                Toast.makeText(Ejercicio2.this, "El resultado ya se ha mostrado. Ingrese una nueva operación.", Toast.LENGTH_SHORT).show();
             }
-            switch(accion){
-                case suma:
-                    val1 = val1 + val2;
-                    break;
-                case resta:
-                    val1 = val1 - val2;
-                    break;
-                case multiplicar:
-                    val1 = val1 * val2;
-                    break;
-                case dividir:
-                        val1 = val1 / val2;
-                    break;
-                case EQU:
-                    break;
-            }
+        });
+
+        borrar.setOnClickListener(v -> {
+           /* if (info.getText().length() > 0) {
+                CharSequence name = info.getText().toString();
+                info.setText(name.subSequence(0, name.length() - 1));
+            } else { */
+                resetCalculator();
+           // }
+        });
+    }
+
+    private void handleNumberInput(String number) {
+        if (isResultDisplayed) {
+            resetCalculator();
         }
-        else{
-            val1 = Double.parseDouble(info.getText().toString());
+        if (info.getText().toString().equals("0")) {
+            info.setText(number);
+        } else {
+            info.setText(info.getText().toString() + number);
+        }
+    }
+
+    private void handleOperatorInput(char operation) {
+        if (isResultDisplayed) {
+            Toast.makeText(Ejercicio2.this, "El resultado ya se ha mostrado. Ingrese una nueva operación.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!info.getText().toString().isEmpty()) {
+            if (isCalculating) {
+                calculateResult();
+            } else {
+                val1 = Double.parseDouble(info.getText().toString());
+                isCalculating = true;
+            }
+            accion = operation;
+            updateResult(String.valueOf(operation));
+            info.setText("0");
+        }
+    }
+
+    private void calculateResult() {
+        if (!info.getText().toString().isEmpty()) {
+            val2 = Double.parseDouble(info.getText().toString());
+            if (accion == '/' && val2 == 0) {
+                Toast.makeText(Ejercicio2.this, "Error: División por 0 no permitida", Toast.LENGTH_LONG).show();
+                resetCalculator();
+                return;
+            }
+            switch (accion) {
+                case '+':
+                    val1 += val2;
+                    break;
+                case '-':
+                    val1 -= val2;
+                    break;
+                case '*':
+                    val1 *= val2;
+                    break;
+                case '/':
+                    val1 /= val2;
+                    break;
+            }
+            String expression = result.getText().toString() + formatResult(val2) + "=";
+            result.setText(expression);
+            info.setText(formatResult(val1));
+            isResultDisplayed = true;
+            isCalculating = false;
+        } else {
+            Toast.makeText(Ejercicio2.this, "Por favor ingrese un valor antes de calcular.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateResult(String operation) {
+        String currentResult = result.getText().toString();
+        if (currentResult.isEmpty()) {
+            result.setText(formatResult(val1) + operation);
+        } else {
+            if (currentResult.matches(".*[\\+\\-\\*\\/]$")) {
+                currentResult = currentResult.substring(0, currentResult.length() - 1);
+            }
+            result.setText(currentResult + operation);
+        }
+    }
+
+    private void resetCalculator() {
+        val1 = Double.NaN;
+        val2 = Double.NaN;
+        result.setText("");
+        info.setText("0");
+        isCalculating = false;
+        isResultDisplayed = false;
+    }
+
+    private String formatResult(double result) {
+        if (result == (long) result) {
+            return String.format("%d", (long) result);
+        } else {
+            return String.format("%s", result);
         }
     }
 }
